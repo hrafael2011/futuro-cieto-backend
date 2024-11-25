ea32be4db7659f36b244f22d2aa18f5df50c48ba
Hendrick Rafael <tuemail@example.com>
2024-11-25 17:53:30 -0500

    modificacion de base de datos para que pueda ser utilizada en azure

--- futuroCiertoApi/settings.py
+++ futuroCiertoApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'futuroCiertoApi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -90,6 +91,19 @@ DATABASES = {
         }
     }
 }
+
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+        }
+    }
+}
+
 
 
 # Password validation
