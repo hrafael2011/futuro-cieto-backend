3dd9e7c03241418bf38a42179a067723ee03875f
Hendrick Rafael <tuemail@example.com>
2024-11-29 09:10:27 -0500

    agregre CSRF_TRUSTED_ORIGINS para que python acepte la entrada al panel de admin

--- futuroCiertoApi/settings.py
+++ futuroCiertoApi/settings.py
@@ -30,6 +30,13 @@ ALLOWED_HOSTS = ['futurociertord.azurewebsites.net',
                  '127.0.0.1']
 
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://futurociertord.azurewebsites.net',  # Dominio de tu app con HTTPS
+    'http://futurociertord.azurewebsites.net',   # Si usas HTTP (temporalmente)
+]
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
